c53277e1521464ee948fa80e58d65c2ac845dcd8
Stéphane Bressani <stephane@bressani.dev>
2022-01-26 09:49:48 +0100

    Astro

--- pull2.sh
+++ pull2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh debian@bressani.dev "cd /home/debian/www/astrologie-traditionnelle.net && ./docker-build.sh && ./docker-run.sh"
+ssh debian@bressani.dev "cd /home/debian/www/astrologie-traditionnelle.net-vuecli && ./docker-build.sh && ./docker-run.sh"
